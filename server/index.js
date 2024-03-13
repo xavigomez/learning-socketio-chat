dad6b6a34a934a3616c39a0179bf44170f8efc30
Xavier Gómez <codegoonie@gmail.com>
2024-03-13 16:11:32 +0100

    add client side conenection

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ const io = new Server(server);
 
 io.on("connection", (socket) => {
   console.log("A user has connected.");
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected");
+  });
 });
 
 app.use(logger("dev"));
